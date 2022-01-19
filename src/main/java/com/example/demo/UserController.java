5cddd93dd025a4ff403e09329dfc769866abe1fe
ZakirGI <sauceteaapple@gmail.com>
2022-01-19 14:05:10 +0530

    MySQL Push

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,4 +1,6 @@
 package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -6,21 +8,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.User;
-@RestController 
+import com.example.demo.service.IUserService;
+
+@RestController
 public class UserController {
+	@Autowired
+	IUserService userService;
+
 	@GetMapping("/user")
-	String getUser(){
+	String getUser() {
 		System.out.println("testing");
 		return "testing";
 	}
+
 	@PostMapping("/user")
 	void createUser(@RequestBody User user) {
 		System.out.println(user.getName());
+		userService.saveUser(user);
 	}
+
 	@DeleteMapping("/user")
 	void deleteUser() {
-		// Commnet Added
-		
+
 	}
- 
 }
